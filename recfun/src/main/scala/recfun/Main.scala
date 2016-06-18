de6bb9ea8b1ecf19449c439bc899d5c8d3be7bd2
andrueastman <andrueastman@gmail.com>
2016-06-18 12:46:15 +0300

    Random files lol

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -20,10 +20,22 @@ object Main {
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
-  
+    def balance(chars: List[Char]): Boolean = {
+      def balanced(chars: List[Char], open: Int): Boolean = {
+          if (chars.isEmpty) open == 0      //return if the expression is balance if at end of list
+          else if (chars.head == '(') balanced(chars.tail,open+1)  //opening bracket found      
+          else if (chars.head == ')') open>0 && balanced(chars.tail,open-1) //closing bracket found(if negative fails)
+          else balanced(chars.tail,open)//move forward
+      }      
+      balanced(chars,0)
+  }
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = {//credit to stack overflow :)
+      if(money == 0)1
+      else if(money > 0 && !coins.isEmpty)
+        countChange(money - coins.head, coins) + countChange(money, coins.tail)
+      else 0
+    }
   }
