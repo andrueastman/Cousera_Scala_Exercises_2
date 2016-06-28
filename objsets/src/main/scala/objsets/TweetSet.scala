7e84097076e2d87da25c51f4e7ca3e4ec636968e
andrueastman <andrueastman@gmail.com>
2016-06-28 14:12:56 +0300

    Filter method works well. I think

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -41,7 +41,7 @@ abstract class TweetSet {
    * Question: Can we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def filter(p: Tweet => Boolean): TweetSet = ???
+    def filter(p: Tweet => Boolean): TweetSet = filterAcc(p, new Empty)
   
   /**
    * This is a helper method for `filter` that propagetes the accumulated tweets.
@@ -107,7 +107,9 @@ abstract class TweetSet {
 }
 
 class Empty extends TweetSet {
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+  
+  
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc //same as returning empty
     
     def union(that: TweetSet): TweetSet = that
   
@@ -125,8 +127,12 @@ class Empty extends TweetSet {
 }
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
+  
 
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = {
+       if(p(elem)) left.filterAcc(p, right.filterAcc(p, acc.incl(elem)))
+       else left.filterAcc(p, right.filterAcc(p, acc))
+    }
   
     def union(that: TweetSet): TweetSet= ((left union right) union that) incl elem
     
